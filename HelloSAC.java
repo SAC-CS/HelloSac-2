9b1fabe623ef36399706153ff6dba31c8e52e475
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-02-21 20:05:24 -0800

    Added files via upload

---
+++ HelloSAC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sherwin
+ */
+public class HelloSAC {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, Santa Ana College or Hello, World!");
+
+
+    }
+    
+}
